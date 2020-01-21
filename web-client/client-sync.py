4261edae8e23527dbafb292749d5bbf654490bf9
Bhushan Mohan <mailtovb@gmail.com>
2020-01-21 01:53:36 +0000

    [web-client] Improve comments and remove emit().
    We add comments to some functions and also remove calls to emit()
    to avoid flooding the console.

--- web-client/client-sync.py
+++ web-client/client-sync.py
@@ -1,7 +1,7 @@
 from time import sleep
 import socketio
 
-url = "http://ec2-18-237-144-210.us-west-2.compute.amazonaws.com:8000"
+url = "http://ec2-35-161-161-253.us-west-2.compute.amazonaws.com:8000"
 sio = socketio.Client()
 
 
@@ -26,17 +26,23 @@ print("My id:", sio.sid)
 
 @sio.event
 def message(data):
+    """
+    Handler for messages sent by the server to the "message" event.
+    """
     print("Event name: message. Message from server:", data)
 
 
 @sio.on("my response")
 def handle_response(data):
+    """
+    Handler for messages sent by the server to the "my response" event.
+    """
     print("Event name: my response. Message from server:", data)
 
 
-for i in range(100):
+for i in range(1):
     sio.emit("message", "Hello server" + str(i))
-    sio.emit("my event", "Hello my event in server" + str(i))
+    # sio.emit("my event", "Hello my event in server" + str(i))
     sleep(0.2)
 
 sio.disconnect()
