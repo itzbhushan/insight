39a6af2f36f019c1d83421cfe9787d5491798cb2
Bhushan Mohan <mailtovb@gmail.com>
2020-01-28 12:55:28 -0800

    [batch-pipeline] Index S3 data into ES using Spark
    In this changeset, we use Apache Spark to read
    data from S3 and index in to AWS Elastic Search (ES).
    
    Offline, we first setup a spark cluster and an elastic
    search cluster. The elastic search cluster URL is
    exposed as an environment variable and we use spark
    to project (get a subset of columns) and index into ES.

---
+++ spark-to-es/index-to-es.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+import json
+import logging
+import os
+
+import findspark
+
+from pyspark.sql import SparkSession, SQLContext, types
+
+logging.basicConfig(level=logging.WARNING)
+STACK_OVERFLOW_SCHEMA = "stackoverflow-schema.json"
+S3_URL = "s3a://stackoverflow-ds"
+ES_INDEX = "so-questions"
+
+
+def main():
+    parser = ArgumentParser("Bulk insert stack overflow questions into elastic search")
+    parser.add_argument("file", help="Regex corresponding to files in s3.")
+    parser.add_argument(
+        "--mapping", help="Path to mapping.json.", default=STACK_OVERFLOW_SCHEMA
+    )
+    parser.add_argument("--s3-url", help="S3 URL prefix", default=S3_URL)
+    parser.add_argument("--es-index", help="Index in ES.", default=ES_INDEX)
+    args = parser.parse_args()
+
+    with open(args.mapping) as f:
+        schema = json.loads(f.read())["mappings"]["properties"]
+        columns = schema.keys()
+
+    findspark.init()
+    spark = SparkSession.builder.appName("write-to-elastic-search").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    sqlContext = SQLContext(sparkContext=spark.sparkContext, sparkSession=spark)
+
+    logging.info("Reading data from S3 and writing to elastic search in progress.")
+
+    data_file = os.path.join(args.s3_url, args.file)
+    df = sqlContext.read.json(data_file)
+
+    # Raw file contains close to a hundred columns (or keys). We
+    # only select a handful of them (columns saved in args.mapping)
+    # and index them in elastic search.
+    df = df.filter(df.type == "question").select(*columns)
+    df.show()
+    df.write.format("org.elasticsearch.spark.sql").option(
+        "es.nodes", os.getenv("ES_URL")
+    ).option("es.port", 443).option("es.resource", args.es_index).option(
+        "es.mapping.id", "id"
+    ).option(
+        "es.nodes.wan.only", True
+    ).mode(
+        "append"
+    ).save()
+
+    logging.info("Completed indexing data into elastic search.")
+
+
+if __name__ == "__main__":
+    main()
